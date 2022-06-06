c047678d8d000e4d199610b645202c6df2d8c6a1
Aaron Isakov <61562774+Grieze@users.noreply.github.com>
2022-06-06 14:40:06 -0400

    Chore/7 move routes to api folders (#16)
    * initial commit, deleted .gitkeep, and created get_coastlines directory
    
    * created new directory path in api for coastlines and started writing routes in coastlines.py
    
    * Moved coastlines route into api/v1/coastlines directory, implemented changes to config.py, added v1.py for potential future use in api/v1 directory
    
    * deleted v1.py from v1 directory, and ran pre-commit checks on all other files.
    
    * Made conflict resolution changes in main and coastlines python files.
    
    * modified config file to get rid of unneccessary class attribtues. Got rid of unused imports in coastlines.py
    
    * chore: restore env-template and fix branch
    
    Co-authored-by: Kyle J. Burda <47502769+kylejb@users.noreply.github.com>

---
+++ server/api/v1/endpoints/coastlines.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import joinedload
+
+from server import models, schemas
+# TODO Implement Settings to have hardcoded route prefixes for future use
+from server.core.config import COASTLINES_V1_STR
+from server.db.session import get_db
+
+router = APIRouter(prefix=COASTLINES_V1_STR, tags=["coastlines"])
+
+
+@router.get("/")
+async def get_coastlines(
+    db_session: AsyncSession = Depends(get_db),
+) -> models.Coastline:
+
+    # `selectinload`: alternative approach to `joinedload`
+    stmt = (
+        select(models.Coastline)
+        .options(joinedload(models.Coastline.buoy, innerjoin=True))
+        .order_by(models.Coastline.id)
+    )
+    response = await db_session.execute(stmt)
+
+    """TODO
+    * Add error handling
+    """
+    return [schemas.Coastline.from_orm(row.Coastline).dict() for row in response]
--- server/core/config.py
+++ server/core/config.py
@@ -6,11 +6,12 @@ from pydantic import BaseSettings
 load_dotenv()
 
 API_V1_STR = "/api/v1"
+COASTLINES_V1_STR = "%s/coastlines" % API_V1_STR
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-
+    # TODO add tags lsit for future development
     class Config:
         env_file = ".env"
 
