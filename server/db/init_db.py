84f2308dd2240153b45e7dc938c4cc6dc9026c75
Kyle J. Burda <kylejbdev@gmail.com>
2022-07-05 20:18:29 -0400

    feat(services): seed buoys with pybuoy (#26)
    * deps: add pybuoy v0.1.1
    
    * deps: remove beautifulsoup4 and lxml
    
    * deps: update all to latest

--- server/db/init_db.py
+++ server/db/init_db.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 # Make sure all SQL Alchemy models are imported before initializing DB
 # https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
 from server.db import base  # noqa: F401
-from server.services.buoy.seed_buoys import NDBCScraper_Buoy
+from server.services.buoy.seed_buoys import seed_active_buoys
 from server.services.coastline.seed_coastline import seed_rockaway
 
 
 async def init_db(db_session: AsyncSession) -> None:
-    await NDBCScraper_Buoy(db_session)
+    await seed_active_buoys(db_session)
     await seed_rockaway(db_session)
--- server/services/buoy/seed_buoys.py
+++ server/services/buoy/seed_buoys.py
@@ -1,70 +1,43 @@
-import requests  # type: ignore # TODO: support mypy
-from bs4 import BeautifulSoup as bs
+from pybuoy import Buoy
 
 from server.crud.crud_buoy import buoy
 from server.schemas.buoy import BuoyCreate
 
 
-async def NDBCScraper_Buoy(db):
-    """NDBC Scraper.
-    Request Parameters for Observations.
-        request = GetObservation
-        service = SOS
-        version = 1.0.0
-        offering = urn:ioos:station:wmo::<station ID> for single station,
-            or urn:ioos:network:noaa.nws.ndbc:all for use with collections.
-        observedproperty = one of the following:
-            air_pressure_at_sea_level
-            air_temperature
-            currents
-            sea_floor_depth_below_sea_surface (water level for tsunami stations)
-            sea_water_electrical_conductivity
-            sea_water_salinity
-            sea_water_temperature
-            waves
-        responseformat=text/csv
-    """
-    ACTIVESTATIONS_XML = "https://www.ndbc.noaa.gov/activestations.xml"
+async def seed_active_buoys(db):
+    # ACTIVESTATIONS_XML = "https://www.ndbc.noaa.gov/activestations.xml"
     # OBS_ENDPOINT = "https://sdf.ndbc.noaa.gov/sos/server.php"
 
-    def get_activestations():
-        """Extract NDBC's activestations from `activestations.xml` endpoint."""
-        xml_data = requests.get(ACTIVESTATIONS_XML).content
-
-        soup = bs(xml_data, "xml")
-        return soup.find_all("station")
-
     def parse_activestations(stations):
-        parsed_stations_obj = []
-        for station in stations:
-            station = station.attrs
-            lon, lat = float(station.get("lon", 0.0)), float(station.get("lat", 0.0))
-            geo = f"POINT({lon} {lat})"
-
-            parsed_stations_obj.append(
-                BuoyCreate(
-                    location=geo,
-                    station_id=station.get("id"),
-                    name=station.get("name"),
-                    owner=station.get("owner"),
-                    elev=station.get("elev", 0.0),
-                    pgm=station.get("pgm"),
-                    type=station.get("type"),
-                    met=station.get("met", ""),
-                    currents=station.get("currents", ""),
-                    water_quality=station.get("waterquality", ""),
-                    dart=station.get("dart", ""),
-                    seq=station.get("seq", None),
-                )
+        return [
+            BuoyCreate(
+                location=f"POINT({float(station.get('lon', 0.0))} {float(station.get('lat', 0.0))})",
+                station_id=station.get("id"),
+                name=station.get("name"),
+                owner=station.get("owner"),
+                elev=station.get("elev", 0.0),
+                pgm=station.get("pgm"),
+                type=station.get("type"),
+                met=station.get("met", ""),
+                currents=station.get("currents", ""),
+                water_quality=station.get("waterquality", ""),
+                dart=station.get("dart", ""),
+                seq=station.get("seq", None),
             )
-        return parsed_stations_obj
+            for station in stations
+        ]
 
     async def persist_parsed(stations):
         # TODO: integrate pydantic layer with model layer
         return await buoy.create_buoys(db, stations)
 
-    async def seed_buoy_data():
-        stations = parse_activestations(get_activestations())
-        return await persist_parsed(stations)
+    async def run():
+        # TODO: fix requirement for station_id when only fetching active stations
+        buoy = Buoy(station_id=None)
+        stations = buoy.stations.get_active()
 
-    return await seed_buoy_data()
+        parsed_stations = parse_activestations(stations=stations)
+
+        return await persist_parsed(parsed_stations)
+
+    return await run()
