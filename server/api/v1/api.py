e7a87768d2cd6acfd95da236130667a198714f76
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2022-09-01 08:28:42 -0400

    chore: remove TODO

--- server/api/v1/api.py
+++ server/api/v1/api.py
@@ -17,13 +17,11 @@ api_router.include_router(buoys.router, prefix=BUOYS_PATH, tags=[PathTags.BUOYS]
 api_router.include_router(
     coastlines.router, prefix=COASTLINES_PATH, tags=[PathTags.COASTLINES]
 )
-# TODO: add routher paths for meteorological and wave
 api_router.include_router(
     meteorological_data.router,
     prefix=METEOROLOGICAL_DATA_PATH,
     tags=[PathTags.METEOROLOGICAL_DATA],
 )
-
 api_router.include_router(
     wave_data.router,
     prefix=WAVE_DATA_PATH,
